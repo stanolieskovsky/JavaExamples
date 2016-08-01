b7bbd9ac2ff0d5634f4bce73b6552b1fecbbd421
Stanislav Lieskovský <stano.lieskovsky@gmail.com>
2016-08-01 23:55:44 +0200

    classes added

---
+++ ForPosam/src/FilesReader.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author liesko3
+ */
+public class FilesReader {
+
+    public FilesReader() {
+ }
+   public void readBoxes(){}
+   public void readRoom(){}
+ 
+}
+    
+
