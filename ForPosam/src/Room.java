47706b84d2da2c802212fb4b3bc2641a475e1d8d
Stanislav Lieskovský <stano.lieskovsky@gmail.com>
2016-08-01 23:54:06 +0200

    new Class for project added

---
+++ ForPosam/src/Room.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author liesko3
+ */
+public class Room {
+// pole o sirke a dlzke iba jedna instancia
+    //ak tam bude # tak v danom mieste vytvorim na indexe hodnotu 
+    // ak . - nula
+    //ak #-1  a ak 
+    //tam vlozim nabytok tak dam 2
+    public Room() {
+    }
+    
+}
