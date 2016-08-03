a0b7b13c0ad752522ef13d7546d943d85fed316d
Stanislav Lieskovský <stano.lieskovsky@gmail.com>
2016-08-03 22:15:43 +0200

    class remaned and new atribut added

---
+++ ForPosam/src/Furniture.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author liesko3
+ */
+public class Furniture {
+ private int[] profile;
+ 
+    public Furniture() {
+    }
+
+    public int[] getProfile() {
+        return profile;
+    }
+
+    public void setProfile(int[] profile) {
+        this.profile = profile;
+    }
+    
+}
