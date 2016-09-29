52a4cf2490c12bf0f7173139b94962db3a6fd36a
Stanislav Lieskovský <stano.lieskovsky@gmail.com>
2016-09-29 21:46:41 +0200

    new atribut and get-set methods added

--- ForPosam/src/Furniture.java
+++ ForPosam/src/Furniture.java
@@ -1,3 +1,5 @@
+
+import java.util.Arrays;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,12 +11,13 @@
  * @author liesko3
  */
 public class Furniture {
-
+    private boolean isIncluded;
     private int[] profile;
     private int height;
     private int width;
     private String name;
     private int weight;
+    
 
     public Furniture() {
     }
@@ -57,6 +60,44 @@ public class Furniture {
 
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + Arrays.hashCode(this.profile);
+        hash = 43 * hash + this.height;
+        hash = 43 * hash + this.width;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Furniture other = (Furniture) obj;
+        if (!Arrays.equals(this.profile, other.profile)) {
+            return false;
+        }
+        if (this.height != other.height) {
+            return false;
+        }
+        if (this.width != other.width) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isIsIncluded() {
+        return isIncluded;
+    }
+
+    public void setIsIncluded(boolean isIncluded) {
+        this.isIncluded = isIncluded;
     }
     
     
