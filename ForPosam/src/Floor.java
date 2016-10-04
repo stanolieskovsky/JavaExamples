b8660fa15102523c34b6250128f1a414eb1de8b7
Stanislav Lieskovský <stano.lieskovsky@gmail.com>
2016-10-04 20:29:44 +0200

    new atribut and get-set methods added

--- ForPosam/src/Floor.java
+++ ForPosam/src/Floor.java
@@ -13,7 +13,17 @@ public class Floor {
     public Floor() {
     }
    private int count; 
+   private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+   
+   
     public int getCount() {
         return count;
     }
